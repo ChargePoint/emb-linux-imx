69cddb21c2fb3761c435ff14bb25af20381ba69d
dwright-cpi <dan.wright@chargepoint.com>
2019-06-07 16:34:59 -0700

    1st attempt at integrating the LSM303C driver into the kernel

---
+++ drivers/input/misc/st/ecompass/ism303dac/ism303dac_acc_i2c.c
@@ -0,0 +1,195 @@
+/*
+ * STMicroelectronics ism303dac_acc_i2c.c driver
+ *
+ * Copyright 2016 STMicroelectronics Inc.
+ *
+ * Lorenzo Bianconi <lorenzo.bianconi@st.com>
+ *
+ * Licensed under the GPL-2.
+ */
+
+#include <linux/err.h>
+#include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
+#include <linux/i2c.h>
+#include <linux/input.h>
+#include <linux/uaccess.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/version.h>
+
+#include "ism303dac_core.h"
+
+#define ISM303DAC_WHO_AM_I_ADDR			0x0f
+#define ISM303DAC_WHO_AM_I_DEF			0x43
+
+/* XXX: caller must hold cdata->lock */
+static int ism303dac_acc_i2c_read(struct st_common_data *cdata,
+				 u8 addr, int len, u8 *data)
+{
+	struct i2c_msg msg[2];
+	struct i2c_client *client = to_i2c_client(cdata->dev);
+
+	msg[0].addr = client->addr;
+	msg[0].flags = client->flags;
+	msg[0].len = 1;
+	msg[0].buf = &addr;
+
+	msg[1].addr = client->addr;
+	msg[1].flags = client->flags | I2C_M_RD;
+	msg[1].len = len;
+	msg[1].buf = data;
+
+	return i2c_transfer(client->adapter, msg, 2);
+}
+
+/* XXX: caller must hold cdata->lock */
+static int ism303dac_acc_i2c_write(struct st_common_data *cdata,
+				  u8 addr, int len, u8 *data)
+{
+	u8 send[len + 1];
+	struct i2c_msg msg;
+	struct i2c_client *client = to_i2c_client(cdata->dev);
+
+	send[0] = addr;
+	memcpy(&send[1], data, len * sizeof(u8));
+	len++;
+
+	msg.addr = client->addr;
+	msg.flags = client->flags;
+	msg.len = len;
+	msg.buf = send;
+
+	return i2c_transfer(client->adapter, &msg, 1);
+}
+
+static struct st_sensor_transfer_function ism303dac_acc_i2c_tf = {
+	.write = ism303dac_acc_i2c_write,
+	.read = ism303dac_acc_i2c_read,
+};
+
+#ifdef CONFIG_PM
+static int ism303dac_acc_i2c_resume(struct device *device)
+{
+	struct i2c_client *client = to_i2c_client(device);
+	struct st_common_data *cdata = i2c_get_clientdata(client);
+
+	return ism303dac_acc_enable(cdata);
+}
+
+static int ism303dac_acc_i2c_suspend(struct device *device)
+{
+	struct i2c_client *client = to_i2c_client(device);
+	struct st_common_data *cdata = i2c_get_clientdata(client);
+
+	return ism303dac_acc_disable(cdata);
+}
+
+static const struct dev_pm_ops ism303dac_acc_i2c_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(ism303dac_acc_i2c_suspend, ism303dac_acc_i2c_resume)
+};
+#endif /* CONFIG_PM */
+
+#ifdef CONFIG_OF
+static const struct of_device_id ism303dac_acc_i2c_id_table[] = {
+	{ .compatible = "st,ism303dac_acc", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, ism303dac_acc_i2c_id_table);
+#endif /* CONFIG_OF */
+
+static int ism303dac_acc_i2c_probe(struct i2c_client *client,
+			     const struct i2c_device_id *id)
+{
+	int err;
+	struct st_common_data *cdata;
+
+#ifdef ISM303DAC_DEBUG
+	dev_info(&client->dev, "probe start.\n");
+#endif
+
+	/* Alloc Common data structure */
+	cdata = kzalloc(sizeof(struct st_common_data), GFP_KERNEL);
+	if (!cdata) {
+		dev_err(&client->dev, "failed to allocate module data\n");
+		return -ENOMEM;
+	}
+
+	cdata->sensors = (struct st_sensor_data *) kmalloc(
+						sizeof(struct st_sensor_data) *
+						ISM303DAC_SENSORS_NUMB,
+						GFP_KERNEL);
+	if (!cdata->sensors)
+		return -ENOMEM;
+
+	cdata->priv_data = (priv_data_t *) kmalloc(sizeof(priv_data_t),
+						   GFP_KERNEL);
+	if (!cdata->priv_data)
+		return -ENOMEM;
+
+	cdata->dev = &client->dev;
+	cdata->name = client->name;
+	cdata->irq = client->irq;
+	cdata->bus_type = BUS_I2C;
+	cdata->tf = &ism303dac_acc_i2c_tf;
+	cdata->wai_addr = ISM303DAC_WHO_AM_I_ADDR;
+	cdata->wai_val = ISM303DAC_WHO_AM_I_DEF;
+
+	mutex_init(&cdata->lock);
+
+	i2c_set_clientdata(client, cdata);
+
+	err = ism303dac_acc_probe(cdata);
+	if (err < 0) {
+		kfree(cdata);
+
+		return err;
+	}
+
+	return 0;
+}
+
+int ism303dac_acc_i2c_remove(struct i2c_client *client)
+{
+	struct st_common_data *cdata = i2c_get_clientdata(client);
+
+#ifdef ISM303DAC_DEBUG
+	dev_info(cdata->dev, "driver removing\n");
+#endif
+
+	ism303dac_acc_remove(cdata);
+	kfree(cdata);
+
+	return 0;
+}
+
+static const struct i2c_device_id ism303dac_acc_i2c_id[] = {
+	{ "ism303dac_acc", 0 },
+	{ },
+};
+MODULE_DEVICE_TABLE(i2c, ism303dac_acc_i2c_id);
+
+static struct i2c_driver ism303dac_acc_i2c_driver = {
+	.driver = {
+		.owner = THIS_MODULE,
+		.name = "ism303dac_acc_i2c",
+#ifdef CONFIG_PM
+		.pm = &ism303dac_acc_i2c_pm_ops,
+#endif /* CONFIG_PM */
+#ifdef CONFIG_OF
+		.of_match_table = ism303dac_acc_i2c_id_table,
+#endif /* CONFIG_OF */
+	},
+	.probe = ism303dac_acc_i2c_probe,
+	.remove = ism303dac_acc_i2c_remove,
+	.id_table = ism303dac_acc_i2c_id,
+};
+
+module_i2c_driver(ism303dac_acc_i2c_driver);
+
+MODULE_DESCRIPTION("ism303dac_acc i2c driver");
+MODULE_AUTHOR("Lorenzo Bianconi <lorenzo.bianconi@st.com>");
+MODULE_LICENSE("GPL v2");
+
---
+++ drivers/input/misc/st/ecompass/ism303dac/ism303dac_acc_spi.c
@@ -0,0 +1,218 @@
+/*
+ * STMicroelectronics ism303dac_acc_spi.c driver
+ *
+ * Copyright 2016 STMicroelectronics Inc.
+ *
+ * Lorenzo Bianconi <lorenzo.bianconi@st.com>
+ *
+ * Licensed under the GPL-2.
+ */
+
+#include <linux/err.h>
+#include <linux/device.h>
+#include <linux/errno.h>
+#include <linux/slab.h>
+#include <linux/spi/spi.h>
+#include <linux/input.h>
+#include <linux/uaccess.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/version.h>
+
+#include "ism303dac_core.h"
+
+#define ISM303DAC_WHO_AM_I_ADDR			0x0f
+#define ISM303DAC_WHO_AM_I_DEF			0x43
+
+#define SENSORS_SPI_READ	0x80
+
+/* XXX: caller must hold cdata->lock */
+static int ism303dac_acc_spi_read(struct st_common_data *cdata,
+				 u8 addr, int len, u8 *data)
+{
+	int err;
+	struct spi_message msg;
+	struct spi_device *spi = to_spi_device(cdata->dev);
+
+	struct spi_transfer xfers[] = {
+		{
+			.tx_buf = cdata->tb.tx_buf,
+			.bits_per_word = 8,
+			.len = 1,
+		},
+		{
+			.rx_buf = cdata->tb.rx_buf,
+			.bits_per_word = 8,
+			.len = len,
+		}
+	};
+
+	cdata->tb.tx_buf[0] = addr | SENSORS_SPI_READ;
+
+	spi_message_init(&msg);
+	spi_message_add_tail(&xfers[0], &msg);
+	spi_message_add_tail(&xfers[1], &msg);
+
+	err = spi_sync(spi, &msg);
+	if (err)
+		return err;
+
+	memcpy(data, cdata->tb.rx_buf, len * sizeof(u8));
+
+	return len;
+}
+
+/* XXX: caller must hold cdata->lock */
+static int ism303dac_acc_spi_write(struct st_common_data *cdata,
+				  u8 addr, int len, u8 *data)
+{
+	struct spi_message msg;
+	struct spi_device *spi = to_spi_device(cdata->dev);
+
+	struct spi_transfer xfers = {
+		.tx_buf = cdata->tb.tx_buf,
+		.bits_per_word = 8,
+		.len = len + 1,
+	};
+
+	if (len >= ISM303DAC_TX_MAX_LENGTH)
+		return -ENOMEM;
+
+	cdata->tb.tx_buf[0] = addr;
+
+	memcpy(&cdata->tb.tx_buf[1], data, len);
+
+	spi_message_init(&msg);
+	spi_message_add_tail(&xfers, &msg);
+	return spi_sync(spi, &msg);
+}
+
+static struct st_sensor_transfer_function ism303dac_acc_spi_tf = {
+	.write = ism303dac_acc_spi_write,
+	.read = ism303dac_acc_spi_read,
+};
+
+#ifdef CONFIG_PM
+static int ism303dac_acc_spi_resume(struct device *device)
+{
+	struct spi_device *spi = to_spi_device(device);
+	struct st_common_data *cdata = spi_get_drvdata(spi);
+
+	return ism303dac_acc_enable(cdata);
+}
+
+static int ism303dac_acc_spi_suspend(struct device *device)
+{
+	struct spi_device *spi = to_spi_device(device);
+	struct st_common_data *cdata = spi_get_drvdata(spi);
+
+	return ism303dac_acc_disable(cdata);
+}
+
+static const struct dev_pm_ops ism303dac_acc_spi_pm_ops = {
+	SET_SYSTEM_SLEEP_PM_OPS(ism303dac_acc_spi_suspend,
+				ism303dac_acc_spi_resume)
+};
+#endif /* CONFIG_PM */
+
+#ifdef CONFIG_OF
+static const struct of_device_id ism303dac_acc_spi_id_table[] = {
+	{ .compatible = "st,ism303dac_acc", },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, ism303dac_acc_spi_id_table);
+#endif /* CONFIG_OF */
+
+static int ism303dac_acc_spi_probe(struct spi_device *spi)
+{
+	int err;
+	struct st_common_data *cdata;
+
+#ifdef ISM303DAC_DEBUG
+	dev_info(&spi->dev, "probe start.\n");
+#endif
+
+	/* Alloc Common data structure */
+	cdata = kzalloc(sizeof(struct st_common_data), GFP_KERNEL);
+	if (!cdata) {
+		dev_err(&spi->dev, "failed to allocate module data\n");
+		return -ENOMEM;
+	}
+
+	cdata->sensors = (struct st_sensor_data *) kmalloc(
+						sizeof(struct st_sensor_data) *
+						ISM303DAC_SENSORS_NUMB,
+						GFP_KERNEL);
+	if (!cdata->sensors)
+		return -ENOMEM;
+
+	cdata->priv_data = (priv_data_t *) kmalloc(sizeof(priv_data_t),
+						   GFP_KERNEL);
+	if (!cdata->priv_data)
+		return -ENOMEM;
+
+	cdata->dev = &spi->dev;
+	cdata->name = spi->modalias;
+	cdata->irq = spi->irq;
+	cdata->bus_type = BUS_SPI;
+	cdata->tf = &ism303dac_acc_spi_tf;
+	cdata->wai_addr = ISM303DAC_WHO_AM_I_ADDR;
+	cdata->wai_val = ISM303DAC_WHO_AM_I_DEF;
+
+	mutex_init(&cdata->lock);
+
+	spi_set_drvdata(spi, cdata);
+
+	err = ism303dac_acc_probe(cdata);
+	if (err < 0) {
+		kfree(cdata);
+
+		return err;
+	}
+
+	return 0;
+}
+
+int ism303dac_acc_spi_remove(struct spi_device *spi)
+{
+	struct st_common_data *cdata = spi_get_drvdata(spi);
+
+#ifdef ISM303DAC_DEBUG
+	dev_info(cdata->dev, "driver removing\n");
+#endif
+
+	ism303dac_acc_remove(cdata);
+	kfree(cdata);
+
+	return 0;
+}
+
+static const struct spi_device_id ism303dac_acc_spi_id[] = {
+	{ "ism303dac_acc", 0 },
+	{ },
+};
+MODULE_DEVICE_TABLE(spi, ism303dac_acc_spi_id);
+
+static struct spi_driver ism303dac_acc_spi_driver = {
+	.driver = {
+		.owner = THIS_MODULE,
+		.name = "ism303dac_acc_spi",
+#ifdef CONFIG_PM
+		.pm = &ism303dac_acc_spi_pm_ops,
+#endif /* CONFIG_PM */
+#ifdef CONFIG_OF
+		.of_match_table = ism303dac_acc_spi_id_table,
+#endif /* CONFIG_OF */
+	},
+	.probe = ism303dac_acc_spi_probe,
+	.remove = ism303dac_acc_spi_remove,
+	.id_table = ism303dac_acc_spi_id,
+};
+
+module_spi_driver(ism303dac_acc_spi_driver);
+
+MODULE_DESCRIPTION("ism303dac_acc spi driver");
+MODULE_AUTHOR("Lorenzo Bianconi <lorenzo.bianconi@st.com>");
+MODULE_LICENSE("GPL v2");
+
