ecd11c0468a5c74cec301d4aefd3f6d80d29f10d
VIKAS GAHLAN <vikas.gahlan@chargepoint.com>
2020-01-16 14:23:56 -0800

    Initial ucb board support changes (device tree, defconfig and build script)

--- buildkernel_imx8.sh
+++ buildkernel_imx8.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
-source ../toolchain/environment-setup-aarch64-poky-linux
+source ../toolchain64/environment-setup-aarch64-poky-linux
 unset LDFLAGS
 cores=`cat /proc/cpuinfo | grep processor | wc -l`
 threads=`expr $cores + 2`
 ARCHTYPE=arm64
-mkdir -p build
-make ARCH=$ARCHTYPE mrproper
-make ARCH=$ARCHTYPE distclean
-#make O=build ARCH=arm imx_v7_pbc_defconfig
-make O=build ARCH=$ARCHTYPE defconfig
-make O=build ARCH=$ARCHTYPE -j $threads
-#make O=build ARCH=$ARCHTYPE dtbs -j $threads
-make O=build ARCH=$ARCHTYPE modules -j $threads
+OUTDIR=build64
+mkdir -p $OUTDIR
+make O=$OUTDIR ARCH=$ARCHTYPE mrproper
+make O=$OUTDIR ARCH=$ARCHTYPE distclean
+make O=$OUTDIR ARCH=$ARCHTYPE ucb_defconfig
+make O=$OUTDIR ARCH=$ARCHTYPE -j $threads
