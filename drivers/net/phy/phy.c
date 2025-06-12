2f3aa9932aa90020cdcae46115d63c8e3db7b732
Geert Lens <geert.lens@chargepoint.com>
2025-06-12 10:27:41 +0200

    net: phy: fix regression with AX88772A PHY driver
    Marek reports that a deadlock occurs with the AX88772A PHY used on the
    ASIX USB network driver:
    
    asix 1-1.4:1.0 (unnamed net_device) (uninitialized): PHY [usb-001:003:10] driver [Asix Electronics AX88772A] (irq=POLL)
    Asix Electronics AX88772A usb-001:003:10: attached PHY driver(mii_bus:phy_addr=usb-001:003:10, irq=POLL)
    asix 1-1.4:1.0 eth0: register 'asix' at usb-12110000.usb-1.4, ASIX AX88772 USB 2.0 Ethernet, a2:99:b6:cd:11:eb
    asix 1-1.4:1.0 eth0: configuring for phy/internal link mode
    
    ============================================
    WARNING: possible recursive locking detected
    6.6.0-rc1-00239-g8da77df649c4-dirty #13949 Not tainted
    --------------------------------------------
    kworker/3:3/71 is trying to acquire lock:
    c6c704cc (&dev->lock){+.+.}-{3:3}, at: phy_start_aneg+0x1c/0x38
    
    but task is already holding lock:
    c6c704cc (&dev->lock){+.+.}-{3:3}, at: phy_state_machine+0x100/0x2b8
    
    This is because we now consistently call phy_process_state_change()
    while holding phydev->lock, but the AX88772A PHY driver then goes on
    to call phy_start_aneg() which tries to grab the same lock - causing
    deadlock.
    
    Fix this by exporting the unlocked version, and use this in the PHY
    driver instead.
    
    Reported-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Fixes: ef113a60d0a9 ("net: phy: call phy_error_precise() while holding the lock")
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
    ---
    Reviewing the other PHY drivers, no others appear impacted, just this
    one.

--- drivers/net/phy/phy.c
+++ drivers/net/phy/phy.c
@@ -779,7 +779,7 @@ EXPORT_SYMBOL_GPL(phy_config_inband_aneg);
  *   If the PHYCONTROL Layer is operating, we change the state to
  *   reflect the beginning of Auto-negotiation or forcing.
  */
-static int _phy_start_aneg(struct phy_device *phydev)
+int _phy_start_aneg(struct phy_device *phydev)
 {
 	int err;
 
@@ -800,6 +800,7 @@ static int _phy_start_aneg(struct phy_device *phydev)
 
 	return err;
 }
+EXPORT_SYMBOL(_phy_start_aneg);
 
 /**
  * phy_start_aneg - start auto-negotiation for this PHY device
